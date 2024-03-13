fdf180a6918889dc8ca5300b3c164b2b4d027854
Gangulaprathapreddy <108295633+Gangulaprathapreddy@users.noreply.github.com>
2024-03-13 11:57:01 +0530

    Update start_container.sh

--- script/start_container.sh
+++ script/start_container.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull gangulaprathapreddy/simple-python-flask-app:tagname
-
+docker pull gangulaprathapreddy/simple-python-flask-app
 # Run the Docker image as a container
-docker run -d -p 5000:5000
+docker run -d -p 5000:5000 gangulaprathapreddy/simple-python-flask-app
